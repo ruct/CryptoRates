c80bde07bc40c0d96f3578de578867d9ee19a4ca
ltry <flyce32@yandex.ru>
2020-01-11 14:09:34 +0300

    exmo init

--- exmo/exmo.go
+++ exmo/exmo.go
@@ -1,1 +1,16 @@
 package exmo
+
+import (
+	"../header"
+)
+
+type Exmo struct {
+
+}
+func (*Exmo) GetName() string {
+	return "exmo"
+}
+
+func (exmo *Exmo) GetRate(currPair header.CurrPair) (header.Rate, error) {
+	return header.Rate{}, nil
+}
